51c87fb587a8a1e19cea668fdcb69d0e83c73d30
nagabhushanam <nagabhushanamn@gmail.com>
2022-01-07 14:31:13 -0500

    intial commit

---
+++ rev-service/src/test/java/com/example/revservice/RevServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.revservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
